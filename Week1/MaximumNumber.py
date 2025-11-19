e6ca4db5c235f92a7639e109d0156330120af215
Priyanka8433 <priyuchaudhary64@gmail.com>
2025-11-19 21:21:27 +0530

    Add files via upload

---
+++ Week1/MaximumNumber.py
@@ -0,0 +1,18 @@
+a=int(input("Enter fisrt number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+
+if(a>b):
+    
+    if(a>c):
+        print(" a is maximum")
+    
+    else:
+        print("c is maximum")
+    
+else :  
+    if b > c:
+        print("b is maximum")
+    else:
+        print("c is maximum")
+    
